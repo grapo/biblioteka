77825e1074c6110b693fae54e318c01e06575009
Piotr Grabowski <grapox@gmail.com>
2011-10-23 18:04:16 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biblioteka::Application.config.secret_token = 'e0fa44781199317784d72acad429e38c118762299a4882153e8281e606b083d9b9ae3aaa2d7a8aff10dac4ea359ba5ff476146f0f6a5ecd99adfc7f1b45b187e'
